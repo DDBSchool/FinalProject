4115fd7e4a50e5a4c7bbf24a9d1e64ac54ca3953
DDBSchool <denmark.bartolome@student.laverdad.edu.ph>
2025-05-25 02:55:26 +0800

    Create Result.java

---
+++ Result.java
@@ -0,0 +1,19 @@
+public class Result {
+    public String username;
+    public int score;
+    public double accuracy;
+    public double wps;
+    public String date;
+
+    public Result(String username, int score, double accuracy, double wps, String date) {
+        this.username = username;
+        this.score = score;
+        this.accuracy = accuracy;
+        this.wps = wps;
+        this.date = date;
+    }
+
+    public int getScore() { return score; }
+    public double getAccuracy() { return accuracy; }
+    public double getWps() { return wps; }
+}
